e3be1b9c805b7ea3ccdcf4dfe856fee03a177252
DerenB <dbozerprogramming@gmail.com>
2024-08-18 15:51:45 -0400

    Working on Navbar

--- portfolio/Components/Navbar.jsx
+++ portfolio/Components/Navbar.jsx
@@ -5,16 +5,19 @@ import '../Styles/Navbar.css'
 
 function NavBar() {
   return (
-    <nav className='navMain'>
-      <div className='navIcon'>
-        Portfolio
-      </div>
-      <ul className='navList'>
-        <li className='navItem'><Link to="/">Home</Link></li>
-        <li className='navItem'><Link to="/about">About</Link></li>
-        <li className='navItem'><Link to="/contact">Contact</Link></li>
-      </ul>
-    </nav>
+    <div className='navMainBackground'>
+      <nav className='navMain'>
+        <div className='navIcon'>
+          Portfolio
+        </div>
+        <ul className='navList'>
+          <li className='navItem'><Link to="/">Home</Link></li>
+          <li className='navItem'><Link to="/about">About</Link></li>
+          <li className='navItem'><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+    </div>
+    
   );
 }
 
--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 // Pages
@@ -11,17 +12,43 @@ import NavBar from '../Components/Navbar.jsx';
 // Styles
 import '../Styles/App.css';
 
-
 function App() {
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    const content = contentRef.current;
+
+    const preventScroll = (event) => {
+      const { scrollTop, scrollHeight, clientHeight} = content;
+
+      if (scrollTop === 0 && event.deltaY < 0) {
+        event.preventDefault();
+      }
+
+      if (scrollTop + clientHeight >= scrollHeight && event.deltaY > 0) {
+        event.preventDefault();
+      }
+    }
+
+    content.addEventListener('wheel', preventScroll)
+
+    return () => {
+      content.removeEventListener('wheel', preventScroll)
+    }
+  }, [])
+
+
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <div className='content' ref={contentRef}>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
