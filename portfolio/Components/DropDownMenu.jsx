df699f119dbfd942e525f0dfaf2e0524720b2f20
DerenB <dbozerprogramming@gmail.com>
2024-09-11 14:33:04 -0400

    Updating NavBar

--- portfolio/Components/DropDownMenu.jsx
+++ portfolio/Components/DropDownMenu.jsx
@@ -6,18 +6,26 @@ function DropDownMenu( { handleClick }) {
     return (
         <div className='menuDropDownContainer'>
           <div className='menuDropDown'>
+
+            {/*
             <Link to="/" className='menuDropDownItem' onClick={handleClick}>
               <div>Home</div>
             </Link>
+            */}
+
             <a href='https://github.com/DerenB' target='_blank' rel="noopener noreferrer" className='menuDropDownItem' onClick={handleClick}>
               GitHub
             </a>
             <a href='https://www.linkedin.com/in/deren-bozer/' target='_blank' rel="noopener noreferrer" className='menuDropDownItem' onClick={handleClick}>
               LinkedIn
-              </a>
+            </a>
+
+            {/*
             <Link to="/contact" className='menuDropDownItem' onClick={handleClick}>
               <div>Contact</div>
             </Link>
+            */}
+
           </div>
         </div>
     )
--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -48,8 +48,9 @@ function App() {
         <NavBar />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
+          {/* <Route path="/about" element={<About />} /> */}
+          {/* <Route path="/contact" element={<Contact />} /> */}
+          <Route path="/*" element={<Home />} />
         </Routes>
         <Footer />
       </BrowserRouter>
