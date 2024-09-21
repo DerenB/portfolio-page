d56f29156ff98908ad6dac43f4aababd2bc86582
DerenB <dbozerprogramming@gmail.com>
2024-09-21 16:54:09 -0400

    Updating the dark color theme

--- portfolio/Components/HomeIntroSection.jsx
+++ portfolio/Components/HomeIntroSection.jsx
@@ -17,17 +17,17 @@ const HomeIntroSection = () => {
 
                 {/* Intro Paragraph */}
                 <div className={`homeIntroParagraph ${theme}`}>
-                    I’m a Computer Science and Accounting graduate with a growing 
-                    passion for programming and web design. With a solid foundation 
-                    in web design, I’ve had the opportunity to build visually 
-                    appealing and user-friendly websites.
+                    I’m a Computer Science and Accounting graduate with a growing passion 
+                    for programming and web design. With a strong foundation in creating 
+                    visually appealing and user-friendly websites, I focus on blending 
+                    functionality with creativity.
                 </div>
                 <div className={`homeIntroParagraph ${theme}`}>
-                    Recently, I’ve begun 
-                    exploring mobile app development, eager to expand my skills and 
-                    tackle new challenges. Browse through my projects to see how I’m 
-                    applying my knowledge and creativity in both web and mobile platforms 
-                    as I continue to develop and refine my abilities.
+                    More recently, I’ve ventured into mobile app development, eager to 
+                    broaden my expertise and tackle new challenges. Browse through my 
+                    projects to see how I’m applying my knowledge and creative skills 
+                    across web and mobile platforms as I continue to develop and refine 
+                    my abilities.
                 </div>
 
             </div>
