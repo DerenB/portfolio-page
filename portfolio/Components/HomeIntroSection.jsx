4582469f4dcf5e87ba90ba33d2c3b3301ea8c305
Deren B <dbozerprogramming@gmail.com>
2024-12-03 21:06:34 -0500

    Changing Intro paragraph wording

--- portfolio/Components/HomeIntroSection.jsx
+++ portfolio/Components/HomeIntroSection.jsx
@@ -17,6 +17,22 @@ const HomeIntroSection = () => {
 
                 {/* Intro Paragraph */}
                 <div className={`homeIntroParagraph ${theme}`}>
+                    After several years of working in accounting, I made the exciting 
+                    decision to transition into software development. Throughout my 
+                    journey, I’ve cultivated a deep passion for problem-solving and 
+                    building efficient, scalable solutions. I specialize in Python, 
+                    JavaScript, and SQL, and I’m always eager to apply my skills to 
+                    create intuitive applications and tools
+                </div>
+                <div className={`homeIntroParagraph ${theme}`}>
+                    This portfolio showcases the projects that highlight my growth as 
+                    a developer, my ability to tackle complex challenges, and my 
+                    commitment to continuous learning. I look forward to exploring 
+                    new opportunities and contributing to innovative software solutions.
+                </div> 
+
+                {/* Prior Intro Paragraph */}
+                {/* <div className={`homeIntroParagraph ${theme}`}>
                     I’m a Computer Science and Accounting graduate with a growing passion 
                     for programming and web design. With a strong foundation in creating 
                     visually appealing and user-friendly websites, I focus on blending 
@@ -28,7 +44,7 @@ const HomeIntroSection = () => {
                     projects to see how I’m applying my knowledge and creative skills 
                     across web and mobile platforms as I continue to develop and refine 
                     my abilities.
-                </div>
+                </div> */}
 
             </div>
 
